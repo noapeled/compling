e7aca4cc44bd5ee12523ea40ed9de10f58838844
Noa Peled <noapeled@gmail.com>
2016-02-13 17:09:32 +0200

    Fix bug in 2c

--- PCFG.py
+++ PCFG.py
@@ -167,8 +167,9 @@ class PCFG(PCFGBase):
             new_rules_2b.append(new_rule)
 
         # 2.c) Adapt rules where A appears on the right-hand side.
-        new_rules_2c = [PCFG.__remove_from_rhs(rule, A, epsilon_rule.probability, seen_epsilon_vars)
-                        for rule in new_rules_2b]
+        new_rules_2c = []
+        for rule in new_rules_2b:
+            new_rules_2c.extend(PCFG.__remove_from_rhs(rule, A, epsilon_rule.probability, seen_epsilon_vars))
 
         # Finally, consolidate every set of rules {X -> u [q_1], ..., X -> u [q_n]}
         # into one rule X -> u [q_1 + ... + q_n]
